bfe55614cdb7c0261c792d6c4c460a54c0e50c0c
Chase Moskal <chasemoskal@gmail.com>
2024-07-10 15:05:39 -0700

    incrediblocks: implemented

--- s/game/board/gameboard.ts
+++ s/game/board/gameboard.ts
@@ -1,1 +1,79 @@
 
+import {Stuff} from "../../tools/stuff"
+import {loop2d, vec2, Vec2, Vec3} from "@benev/toolbox"
+
+export class Tile {
+	elevation = 0
+}
+
+export class Gameboard {
+	readonly extent: Vec2 = [8, 8]
+	tiles = [...loop2d(this.extent)].map(() => new Tile())
+
+	at([x, y]: Vec2) {
+		const index = (y * this.extent[0]) + x
+		const tile = this.tiles[index]
+		if (!tile) throw new Error(`tile not found at [${x}, ${y}]`)
+		return tile
+	}
+
+	;*loop() {
+		for (const location of loop2d(this.extent))
+			yield {location, tile: this.at(location)}
+	}
+}
+
+type RenderOptions = {
+	stuff: Stuff
+	gameboard: Gameboard
+	blockSize: number
+	blockHeight: number
+	blockNames: string[]
+}
+
+export class BoardRenderer {
+	#disposers: (() => void)[] = []
+
+	constructor(private options: RenderOptions) {}
+
+	dispose() {
+		this.#disposers.forEach(d => d())
+		this.#disposers = []
+	}
+
+	location([x, y]: Vec2) {
+		const {gameboard, blockSize, blockHeight} = this.options
+		const tile = gameboard.at([x, y])
+		const [offsetX, offsetY] = vec2.divideBy(gameboard.extent, 2)
+		const worldX = (x - offsetX + 0.5) * blockSize
+		const worldY = (tile.elevation * blockHeight) + 1
+		const worldZ = (y - offsetY + 0.5) * blockSize
+		return [worldX, worldY, worldZ] as Vec3
+	}
+
+	#spawnBlock(location: Vec2, elevation: number) {
+		const {stuff, blockNames, blockHeight} = this.options
+		const instance = stuff.instanceProp(blockNames[elevation])
+		const [worldX,, worldZ] = this.location(location)
+		const height = elevation * blockHeight
+		instance.position.set(worldX, height, worldZ)
+		this.#disposers.push(() => instance.dispose())
+	}
+
+	render() {
+		this.dispose()
+		const {gameboard} = this.options
+
+		for (const {location, tile} of gameboard.loop()) {
+			if (tile.elevation >= 0)
+				this.#spawnBlock(location, 0)
+
+			if (tile.elevation >= 1)
+				this.#spawnBlock(location, 1)
+
+			if (tile.elevation >= 2)
+				this.#spawnBlock(location, 2)
+		}
+	}
+}
+
--- s/game/flows/freeplay.ts
+++ s/game/flows/freeplay.ts
@@ -6,6 +6,7 @@ import {DirectionalLight, Vector3} from "@babylonjs/core"
 import {World} from "../world/world.js"
 import {Stuff} from "../../tools/stuff.js"
 import {Orbitcam} from "../orbitcam/orbitcam.js"
+import { BoardRenderer, Gameboard } from "../board/gameboard.js"
 
 const {degrees} = scalar.radians.from
 
@@ -23,8 +24,28 @@ export async function freeplayFlow() {
 	console.log("props", [...stuff.props.values()].map(m => m.name))
 	console.log("meshes", [...stuff.meshes.values()].map(m => m.name))
 
-	stuff.instanceProp("pawn")
+	const pawn = stuff.instanceProp("pawn")
 	stuff.instanceProp("border8x8")
+
+	const gameboard = new Gameboard()
+	gameboard.at([2, 2]).elevation = 1
+	gameboard.at([2, 3]).elevation = 1
+	gameboard.at([3, 2]).elevation = 1
+	gameboard.at([3, 3]).elevation = 2
+
+	const boardRenderer = new BoardRenderer({
+		stuff,
+		gameboard,
+		blockSize: 2,
+		blockHeight: 1,
+		blockNames: [
+			"grid-floor-bottom",
+			"grid-floor-middle",
+			"grid-floor-top",
+		],
+	})
+	boardRenderer.render()
+	pawn.position.set(...boardRenderer.location([3, 3]))
 
 	const orbitcam = new Orbitcam({
 		scene,
