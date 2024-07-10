85301efa9be8ae0a0f6887acf64cc61bf6389cb8
Chase Moskal <chasemoskal@gmail.com>
2024-07-10 14:09:14 -0700

    implement: working orbitcam

--- s/game/flows/freeplay.ts
+++ s/game/flows/freeplay.ts
@@ -1,9 +1,13 @@
 
-import {make_envmap} from "@benev/toolbox"
-import {ArcRotateCamera, DirectionalLight, Vector3} from "@babylonjs/core"
+import {ev} from "@benev/slate"
+import {make_envmap, scalar} from "@benev/toolbox"
+import {DirectionalLight, Vector3} from "@babylonjs/core"
 
 import {World} from "../world/world.js"
 import {Stuff} from "../../tools/stuff.js"
+import {Orbitcam} from "../orbitcam/orbitcam.js"
+
+const {degrees} = scalar.radians.from
 
 export async function freeplayFlow() {
 
@@ -20,17 +24,18 @@ export async function freeplayFlow() {
 	console.log("meshes", [...stuff.meshes.values()].map(m => m.name))
 
 	stuff.instanceProp("pawn")
+	stuff.instanceProp("border8x8")
 
-	const orbitcam = (() => {
-		const name = "name"
-		const alpha = 0
-		const beta = Math.PI * (1 / 3)
-		const radius = 20
-		const target = new Vector3(0, 0, 0)
-		return new ArcRotateCamera(name, alpha, beta, radius, target, scene)
-	})()
-
-	world.rendering.setCamera(orbitcam)
+	const orbitcam = new Orbitcam({
+		scene,
+		sensitivity: 1 / 100,
+		smoothing: 5,
+		verticalRange: [degrees(0), degrees(70)],
+		verticalRadii: [30, 5],
+	})
+	world.rendering.setCamera(orbitcam.camera)
+	const unbindOrbitControls = ev(world.canvas, orbitcam.events)
+	const stopOrbitTick = world.gameloop.on(orbitcam.tick)
 
 	const sun = new DirectionalLight(
 		"sun",
@@ -39,12 +44,6 @@ export async function freeplayFlow() {
 	)
 
 	sun.intensity = .2
-
-	world.gameloop.on(() => {
-		orbitcam.alpha += 0.01
-		return () => {}
-	})
-
 	world.gameloop.start()
 
 	return {
@@ -54,6 +53,9 @@ export async function freeplayFlow() {
 		dispose: () => {
 			envmap.dispose()
 			world.dispose()
+			stopOrbitTick()
+			unbindOrbitControls()
+			orbitcam.dispose()
 		},
 	}
 }
---
+++ s/game/orbitcam/orbitcam.ts
@@ -0,0 +1,95 @@
+
+import {molasses2d, scalar, Vec2} from "@benev/toolbox"
+import {ArcRotateCamera, Scene, Vector3} from "@babylonjs/core"
+
+const {degrees} = scalar.radians.from
+
+type Options = {
+	scene: Scene
+	sensitivity: number
+	smoothing: number
+	verticalRange: Vec2
+	verticalRadii: Vec2
+}
+
+export class Orbitcam {
+	readonly camera: ArcRotateCamera
+	dispose: () => void
+
+	#down = false
+	#gimbal: Vec2 = [0, 0]
+	#smoothedGimbal: Vec2 = [0, 0]
+
+	constructor(private options: Options) {
+		const name = "orbitcam"
+		const alpha = 0
+		const beta = 0
+		const radius = 20
+		const target = new Vector3(0, 1, 0)
+		this.camera = new ArcRotateCamera(name, alpha, beta, radius, target, options.scene)
+		this.dispose = () => this.camera.dispose()
+		this.gimbal = [0, degrees(45)]
+	}
+
+	get gimbal() {
+		return this.#gimbal
+	}
+
+	set gimbal([x, y]: Vec2) {
+		this.#gimbal = [
+			x,
+			scalar.clamp(
+				y,
+				...this.options.verticalRange,
+			),
+		]
+	}
+
+	tick = () => {
+		this.#smoothedGimbal = molasses2d(
+			this.options.smoothing,
+			this.#smoothedGimbal,
+			this.#gimbal,
+		)
+		this.#setCameraToGimbal(this.#smoothedGimbal)
+	}
+
+	#setCameraToGimbal([x, y]: Vec2) {
+		this.camera.alpha = x
+		this.camera.beta = y
+		this.camera.radius = scalar.remap(
+			y,
+			this.options.verticalRange,
+			this.options.verticalRadii,
+		)
+	}
+
+	events = {
+		pointerdown: (_event: PointerEvent) => {
+			this.#down = true
+		},
+
+		pointermove: (event: PointerEvent) => {
+			if (this.#down) {
+				const [x, y] = this.gimbal
+				this.gimbal = [
+					x + (event.movementX * this.options.sensitivity),
+					y - (event.movementY * this.options.sensitivity),
+				]
+			}
+		},
+
+		pointerup: (_event: PointerEvent) => {
+			this.#down = false
+		},
+
+		blur: () => {
+			this.#down = false
+		},
+
+		pointerleave: () => {
+			this.#down = false
+		},
+	}
+}
+
