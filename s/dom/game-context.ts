3c8ca3cc2edfbc61ae3e9bfda9dcb4e1f712bbbb
Chase Moskal <chasemoskal@gmail.com>
2024-08-27 10:25:06 -0700

    cleanup: old obsolete code

--- s/dom/game-context.ts
+++ s/dom/game-context.ts
@@ -4,12 +4,10 @@ import {Context, signal} from "@benev/slate"
 import {theme} from "./theme.js"
 import {InputMethod} from "./utils/input-method.js"
 import {Connectivity} from "../net/connectivity.js"
-// import {MatchmakingLiaison} from "../net/matchmaking-liaison.js"
 
 export class GameContext extends Context {
 	theme = theme
 	inputMethod = signal<InputMethod>("none")
 	connectivity = new Connectivity()
-	// matchmaking = new MatchmakingLiaison(this.connectivity)
 }
 
