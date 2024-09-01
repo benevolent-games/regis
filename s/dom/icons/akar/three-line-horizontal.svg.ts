8f8bcd51ca79be56556df4e6ebbf8a92de0b38a3
Chase Moskal <chasemoskal@gmail.com>
2024-09-01 12:05:53 -0700

    icons: add new ones, plus akar dir

---
+++ s/dom/icons/akar/three-line-horizontal.svg.ts
@@ -0,0 +1,7 @@
+
+import {svg} from "@benev/slate"
+
+export default svg`
+	<svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="ai ai-ThreeLineHorizontal" style="--darkreader-inline-stroke: currentColor;" data-darkreader-inline-stroke=""><path d="M5 6h14M5 12h14M5 18h14"/></svg>
+`
+
